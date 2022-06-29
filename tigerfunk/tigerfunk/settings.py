59f27a867d6ee810b4e5decd902ff5e5eac5b120
Stéphane MEYER <stm3y3r@icloud.com>
2022-06-29 14:44:15 +0200

    updated

--- tigerfunk/tigerfunk/settings.py
+++ tigerfunk/tigerfunk/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'tigerfunk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app_data/static/'
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
