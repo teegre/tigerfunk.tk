f2e357d06884bce2def7e18f4e5d9df690d8f7d2
Stéphane MEYER <tigerlost@me.com>
2022-07-09 14:22:56 +0200

    changed static files location

--- tigerfunk/tigerfunk/settings.py
+++ tigerfunk/tigerfunk/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = '/static/'
+STATIC_ROOT = './static/'
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
