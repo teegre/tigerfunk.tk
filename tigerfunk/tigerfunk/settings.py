0f9929e99f977b78c4c8e0281ce49c15f96e5bc0
Stéphane MEYER <tigerlost@me.com>
2023-10-10 14:43:19 +0200

    settings: updated.

--- tigerfunk/tigerfunk/settings.py
+++ tigerfunk/tigerfunk/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_ROOT = './static/'
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
